c9064bb5478c1e436d71197d8478fa2b299676b7
AtsushiNishimuraRelic <atsushi.nishimura@relic.co.jp>
2024-02-20 11:58:55 +0900

    viteに指定していたCSSファイルのパスを修正。

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/js/app.css',
+                'resources/css/app.css',
                 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
