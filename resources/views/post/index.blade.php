8a8944ad2c8319e4e34d1db883a3f2a87b9a6486
mahhawarn <ba0930fk@ed.ritsumei.ac.jp>
2024-02-21 18:54:22 +0900

    二者択一アンケート

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('投稿一覧') }}
+            {{ __('迷ったら炎上🔥二者択一アンケート！！') }}
         </h2>
     </x-slot>
 
