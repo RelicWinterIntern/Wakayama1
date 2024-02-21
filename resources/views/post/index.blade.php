4790350c70993ca7aeb7ea87bcacffdfc856175a
mahhawarn <ba0930fk@ed.ritsumei.ac.jp>
2024-02-21 22:19:28 +0900

    text color

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('迷ったら炎上🔥二者択一アンケート！！') }}
+            {{ __('迷ったら炎上🔥The二択アンケート！！') }}
         </h2>
     </x-slot>
 
